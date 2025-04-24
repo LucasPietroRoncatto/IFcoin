9a1a75f66c53f92e6e24b470afb749aa492023b7
Lucas Pietro Roncatto <lucasroncatto.va017@academico.ifsul.edu.br>
2025-04-23 21:36:07 -0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -36,7 +36,7 @@ $days_remaining = 45 - $days_since_change;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IFCoin - Painel</title>
+    <title>IFCoin - Painel Principal</title>
     <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
